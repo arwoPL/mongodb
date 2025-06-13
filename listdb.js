9c7b1f1fc6acb9ce2b3f1b25cd45b7cd953df4ce
Arek <arek.w2@gmail.com>
2025-06-13 12:56:35 +0200

    brakujacy plik listdb.js

---
+++ listdb.js
@@ -0,0 +1,24 @@
+const mongo = require("mongodb");
+const MongoClient = mongo.MongoClient
+
+async function processDB() {
+    const url = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(url);
+
+    try {
+        await client.connect();
+
+        const dbList = await client.db().admin().listDatabases();
+
+        console.log("Databases:");
+        dbList.databases.forEach( db => console.log(db.name));
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.close(); 
+    }
+}
+
+//start
+processDB();
